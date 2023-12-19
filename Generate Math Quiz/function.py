f62910e5155b4b629bee4d7f36b9247b9d0ca6af
raflyqowi <raflycow10@gmail.com>
2023-12-19 19:18:28 +0700

    Update using Inference HF API

--- Generate Math Quiz/function.py
+++ Generate Math Quiz/function.py
@@ -2,26 +2,34 @@ from langchain.prompts import PromptTemplate
 from langchain.llms import CTransformers
 from langchain.chains import LLMChain
 from langchain.chains import SequentialChain
+from langchain.llms import HuggingFaceHub
+from dotenv import load_dotenv
 
-config = {'max_new_tokens': 256, 'temperature': 0.1}
+load_dotenv();
+config = {'max_new_tokens': 512, 'temperature': 0.6}
 
 # Create function for app
 def GetLLMResponse(selected_topic_level, 
                     selected_topic,
                     num_quizzes):
-
+    
     # Calling llama model
-    llm = CTransformers(model="D:\Code Workspace\DL Model\llama-2-7b-chat.ggmlv3.q8_0.bin",
-                        model_type = 'llama',
-                        config = config)
+    # llm = CTransformers(model="D:\Code Workspace\DL Model\llama-2-7b-chat.ggmlv3.q8_0.bin",
+    #                     model_type = 'llama',
+    #                     config = config)
 
     # llm = CTransformers(model='TheBloke/Llama-2-7B-Chat-GGML',
     #                     model_file = 'llama-2-7b-chat.ggmlv3.q8_0.bin',
     #                     model_type = 'llama',
     #                     config = config)
+
+    llm = HuggingFaceHub(
+        repo_id = "mistralai/Mixtral-8x7B-Instruct-v0.1",
+        model_kwargs = config
+    )
     
     ## Create LLM Chaining
-    questions_template = "Generate a {selected_topic_level} math quiz on the topic of {selected_topic}. Include {num_quizzes} questions without providing answers."
+    questions_template = "I want you to just generate question with this specification: Generate a {selected_topic_level} math quiz on the topic of {selected_topic}. Generate only {num_quizzes} questions not more and without providing answers. The Question should not in image format/link"
     questions_prompt = PromptTemplate(input_variables=["selected_topic_level", "selected_topic", "num_quizzes"],
                                       template=questions_template)
     questions_chain = LLMChain(llm= llm,
@@ -29,7 +37,7 @@ def GetLLMResponse(selected_topic_level,
                                output_key = "questions")
 
 
-    answer_template = "From this Question:\n {questions}\n\n gave me answer to each one of them"
+    answer_template = "I want you to become a teacher answer this specific Question:\n {questions}\n\n. You should gave me a straightforward and consise explanation and answer to each one of them"
     answer_prompt = PromptTemplate(input_variables = ["questions"],
                                    template = answer_template)
     answer_chain = LLMChain(llm = llm,
